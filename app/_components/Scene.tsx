efa9b8a8b425669b350deac87028419cd0167d21
Will Maddicott <willmaddicott@gmail.com>
2026-06-20 11:41:17 +0100

    lighting tweak

--- app/_components/Scene.tsx
+++ app/_components/Scene.tsx
@@ -17,7 +17,7 @@ const Scene = (props: Props) => {
 
         <ambientLight intensity={0.6} />
         <pointLight intensity={120} position={[10, 10, 10]} />
-        <spotLight intensity={70} position={[-10, -10, -10]} />
+        <spotLight intensity={270} position={[-10, 10, -10]} />
         <PerspectiveCamera makeDefault position={[0, 0, 5]} />
         <OrbitControls />
       </Canvas>
