6476098e6100601dec33564d39bb21e657214161
Will Maddicott <willmaddicott@gmail.com>
2026-06-20 11:50:21 +0100

    notes on route hander

--- app/api/[filename]/route.ts
+++ app/api/[filename]/route.ts
@@ -7,12 +7,14 @@ export const GET = async ({
 }: {
   params: Promise<{ filename: string }>;
 }) => {
+  //TODO: Add validation to ensure the filename is safe and does not allow access to unintended files.
   const { filename } = await params;
 
   const filePath = path.join(process.cwd(), "app/data", filename);
 
   const nodeStream = createReadStream(filePath);
   const webStream = Readable.toWeb(nodeStream) as ReadableStream;
+  //TODO: Add error handling for cases where the file does not exist, can't be read or is interrupted.
 
   return new Response(webStream, {
     headers: {
