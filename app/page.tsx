ceafe965ae35878f56988ce71dc5362f6959c5fd
Will Maddicott <willmaddicott@gmail.com>
2026-06-21 18:24:31 +0100

    meta

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import type { Metadata } from "next";
+
 import DebugToggle from "./_components/DebugToggle";
 import Scene from "./_components/Scene";
 
+export const metadata: Metadata = {
+  title: "Point Cloud Viewer",
+  description:
+    "Interactive point cloud viewer built with React Three Fiber and Three.js.",
+};
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Scene />
       <DebugToggle />
     </div>
